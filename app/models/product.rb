0ec5111f06a338b85bc10f085f1b2df67161473c
Alex Mantel <alexmantel93@gmail.com>
2015-01-14 15:55:32 +0100

    Add presence validation for images

--- app/models/product.rb
+++ app/models/product.rb
@@ -25,7 +25,7 @@ class Product < ActiveRecord::Base
   # validates makes sure that attributes are
   # present. otherwise our view says that there
   # is something missing :)
-  validates_presence_of :name, :description, :price, :amount, :since, :until
+  validates_presence_of :name, :description, :price, :amount, :since, :until, :picture
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 
   def self.search(product)
@@ -51,14 +51,18 @@ class Product < ActiveRecord::Base
     if !(0.1 <= alpha && alpha <= 0.3) then
       raise "alpha isn't between 0.1 and 0.3"
     end
+    _exponential_smoothing(m, alpha)
+  end
+
+ private
+  
+  def _exponential_smoothing(m = 0, alpha = 0.2)
     last_month = (m+1).months.ago.month
     b = need(last_month..last_month)
     pb = (m == 12) ? 0 : exponential_smoothing(last_month)
     pb + (alpha * (b - pb))
   end
 
- private
-  
   def need(m)
     positions.reduce(0) do |accu, pos|
       if pos.order.date.month.in?(m) then
